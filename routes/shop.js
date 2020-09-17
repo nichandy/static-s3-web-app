b27404d0dcbf0c072a4e3a0df835305d5bb40fc8
Nick Handy <nichandy@gmail.com>
2020-09-16 20:18:32 -0700

    Update upon completing ch. 6 ejs

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // automatically builds the absolute path
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
+
+//    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // automatically builds the absolute path
