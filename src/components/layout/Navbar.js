259f74805313925a3e451041e9b902c3672f094d
Nick Handy <nichandy@gmail.com>
2020-12-27 20:34:37 -0800

    Create a biolerplate create react app

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3 py--0">
+          <div className="container">
+            <a href="/" className="navbar-brand">
+              Logo
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#mobile-nav"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="mobile-nav">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <a href="/" className="nav-link">
+                    Home
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
