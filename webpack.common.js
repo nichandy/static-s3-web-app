17484eaa6e375578f3a0d7f99953f294afd9d552
Nick Handy <nichandy@gmail.com>
2021-01-18 22:02:54 -0800

    removing unused packages

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
