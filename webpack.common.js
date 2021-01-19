0afaed905be847fd2c4c8dea1ede4b330e8ede30
Nick Handy <nichandy@gmail.com>
2021-01-18 22:24:35 -0800

    absolute path for webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src') + '/app.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: 'app.js',
   mode: 'development',
   output: {
     path: path.join(__dirname, 'public'),
