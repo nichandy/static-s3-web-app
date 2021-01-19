2f64c64d44e60a33834a0dfebe129a9a62fab3d2
Nick Handy <nichandy@gmail.com>
2021-01-19 00:42:34 -0800

    Add format to currency on expenselistitem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <tr>
     <td>
       <Link to={`/edit/${id}`}>{description}</Link>
     </td>
-    <td>{amount}</td>
+    <td>{numeral(amount / 100).format('$0,0.00')}</td>
     <td>{moment(createdAt).format('MMM Do YYYY, h:mm:ss a')}</td>
   </tr>
 );
